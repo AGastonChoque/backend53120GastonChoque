5a7626600578bfa1f710e6b8f2656b1e50e83bbd
Gaston Choque <gastonchoque95@gmail.com>
2024-08-17 19:53:37 -0300

    add posrt

--- PF/SRC/app.js
+++ PF/SRC/app.js
@@ -82,9 +82,9 @@ app.use('/api/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 
 
-const PORT = config.port
+const PORT = process.env.port
 const httpServer = app.listen(PORT, () => {
-  console.log(`Servidor activo en http://localhost:${PORT}`);
+  console.log(`Servidor activo en ${PORT}`);
 });
 
 const io = new Server(httpServer)
