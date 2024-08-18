4f81e51d8f6825ab116ac040b552f000a65603df
Gaston Choque <gastonchoque95@gmail.com>
2024-08-18 02:53:30 -0300

    getUsersFixName

--- PF/SRC/dao/DTOs/usersDTOGet.js
+++ PF/SRC/dao/DTOs/usersDTOGet.js
@@ -1,6 +1,6 @@
 export default class usersDTOGet {
     constructor(user) {
-        this.first_name = user.first_name;
+        this.complete_name = user.complete_name;
         this.email = user.email;
         this.role = user.role;
     }
